1e365c52d1bd0893e1bf22afb2973eaea541ef52
lavy2p <lavy28p@gmail.com>
2021-07-20 15:59:17 -0500

    updated express controller for fetching a specifc product from mongodb

--- controllers/products.js
+++ controllers/products.js
@@ -8,3 +8,17 @@ export const getProducts = async (req, res) => {
     res.status(500).send({ error: error.message })
   }
 }
+
+export const getProduct = async (req, res) => {
+  try {
+    const { _id } = req.params
+    const product = await Product.findById(_id).populate('userId')
+    if (product) {
+      res.json(product)
+    }
+    res.status(404).json({ message: 'Product not found!' })
+  } catch (error) {
+    console.log(error.message)
+    res.status(500).send({ error: error.message })
+  }
+}
