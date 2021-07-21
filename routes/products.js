b20ac47f8cbcbdd9b2fc1227d885333686dab075
lavy2p <lavy28p@gmail.com>
2021-07-21 09:16:33 -0500

    updated code with successful CRUD in local

--- routes/products.js
+++ routes/products.js
@@ -1,11 +1,14 @@
 import { Router } from 'express'
 import * as controllers from '../controllers/products.js'
-// import restrict pending
+// import restrict pending 
 
 const router = Router()
 
 router.get('/products', controllers.getProducts)
 router.get('/products/:id', controllers.getProduct)
+router.post('/products', controllers.createProduct)
+router.put('/products/:id', controllers.updateProduct)
+router.delete('/products/:id', controllers.deleteProduct)
 
 
 export default router
