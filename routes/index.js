9a791f68df9685e3de9dd1e1c2dbb5bed2a725e6
lavy2p <lavy28p@gmail.com>
2021-07-21 11:00:04 -0500

    updated package.json and files for signin and signup auth.Successful testing in postman

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import productsRoutes from './products.js'
+import userRoutes from './users.js'
 
 const router = Router()
 
 router.get('/', (req, res) => res.send('This is the api root!'))
 
 router.use('/', productsRoutes)
+router.use('/', userRoutes)
 
 export default router
